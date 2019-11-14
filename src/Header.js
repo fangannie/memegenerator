d0ce881a1d5641cee814a98ccf2b75122bd87850
fangannie <39583104+fangannie@users.noreply.github.com>
2019-11-13 18:59:29 -0600

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <p>a very average meme generator</p>
+        </header>
+    )
+}
+
+export default Header
